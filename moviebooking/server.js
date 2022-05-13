30256f9197ef6a6888588fc7d5b4ff94da1f09ef
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2022-05-13 13:55:23 +0530

    Created API and tested it using Express and CORS

--- moviebooking/server.js
+++ moviebooking/server.js
@@ -1,65 +1,53 @@
 // Importing necessary modules
 const http = require('http');
 const status = require('http-status-codes');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
 // The port number on which the server is going to listen
-const PORT = 9000;
+const PORT = 3000;
 // Status codes used for callbacks
 const OK = status.StatusCodes.OK; // 200
 const NOT_FOUND = status.StatusCodes.NOT_FOUND; // 404
+// CORS settings
+const cors_opt = {
+    origin: `127.0.0.1:${PORT}`
+};
+// express middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors(cors_opt));
 
-// TODO: CONNECT TO Mongodb INSTANCE AND CHECK THE CONNECTION
-// AND VALIDATE IF CONFIG IS CORRECTLY DONE
+// TODO: CREATE APP USING EXPRESS AND CORS
+// AND DYNAMICALLY CREATE AND FETCH DATA FROM API
 
-const db = require("./models");
-// This will try to connecct to the DB hosted at mongodb://localhost:27017/moviesdb
-// which is specified inside the controller file. If connection succeeds, then 
-// 'connected to database' is logged in console else the error is thrown
-db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to the database!");
-
-    })
-    .catch(err => {
-        console.log("Cannot connect to the database!", err);
-        process.exit();
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to Upgrad Movie booking application development.",
     });
-// const myServer = http.createServer((req, res) => {
-//     // Parse the url entered by user
-//     let pathname = req.url;
-//     // redirect the user to the specified pages
-//     if (pathname === "/" || pathname === " ") {
-//         res.writeHead(OK, {
-//             "Content-Type": "text/html"
-//         });
-//         res.end("<h1>Welcome to the server</h1>");
-//     } else if (pathname === "/movies") {
-//         res.writeHead(OK, {
-//             "Content-Type": "text/html"
-//         });
-//         res.end("All Movies Data in JSON format from Mongo DB");
-//     } else if (pathname === "/genres") {
-//         res.writeHead(OK, {
-//             "Content-Type": "text/html"
-//         });
-//         res.end("All Genres Data in JSON format from Mongo DB");
-//     } else if (pathname === "/artists") {
-//         res.writeHead(OK, {
-//             "Content-Type": "text/html"
-//         });
-//         res.end("All Artists Data in JSON format from Mongo DB");
-//     } else {
-//         res.writeHead(NOT_FOUND, {
-//             "Content-Type": "text/html"
-//         });
-//         res.end("404 PAGE NOT FOUND");
-//     }
+});
 
-// });
 
-// myServer.listen(PORT, '127.0.0.1', () => {
-//     console.log(`Sever started and is listening on port: ${PORT}`);
-// });
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Server has started and is listening on port: ${PORT}`);
+});
+// const db = require("./models");
+// // This will try to connecct to the DB hosted at mongodb://localhost:27017/moviesdb
+// // which is specified inside the controller file. If connection succeeds, then 
+// // 'connected to database' is logged in console else the error is thrown
+// db.mongoose
+//     .connect(db.url, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => {
+//         console.log("Connected to the database!");
+
+//     })
+//     .catch(err => {
+//         console.log("Cannot connect to the database!", err);
+//         process.exit();
+//     });
